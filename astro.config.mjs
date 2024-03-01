0d63b7b35b27b41b0adb3d47d8f558c9b0a868ec
yunhwan <jimmy53120488@gmail.com>
2024-02-29 21:34:20 -0500

    fix: add base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gitlab.com/aaron-jeong',
+  base: '/designer-wife-portfolio',
   outDir: 'public',
   publicDir: 'static',
 });
