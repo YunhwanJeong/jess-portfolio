8105f210c0819e8505cbb45b48c493c7fadd2b29
yunhwan <jimmy53120488@gmail.com>
2024-03-03 17:03:53 -0500

    chore: change deployment service from gitlab to vercel
    - move static folder to public
    - remove astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://aaron-jeong.gitlab.io',
-  base: '/designer-wife-portfolio',
-  outDir: 'public',
-  publicDir: 'static',
-});
+export default defineConfig();
