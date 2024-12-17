2c7ba07bb3c64f43a565d1ca9f3717492c3cbed0
Aaron Jeong <jimmy53120488@gmail.com>
2024-12-16 21:20:59 -0500

    feat: update project categories tabs with new images and styles
    - Added new styles for project links, including hover effects and image handling.
    - Replaced old images with new video formats for better performance.
    - Removed unused images to clean up the project structure.

--- src/components/ProjectCategoriesTabs.tsx
+++ src/components/ProjectCategoriesTabs.tsx
@@ -1,35 +1,35 @@
-import AimmoPaymentsImage from '@images/aimmo-payments.png';
-import AimmoRevampImage from '@images/aimmo-revamp.png';
-import BuzzerBeaterMainImage from '@images/buzzer-beater-main.png';
-import OrderSpotAppImage from '@images/orderspot-app.png';
-import * as Tabs from '@radix-ui/react-tabs';
-import Styles from './ProjectCategoriesTabs.module.css';
+import AimmoPaymentsImage from "@images/aimmo-payments.png";
+import AimmoRevampImage from "@images/aimmo-revamp.png";
+import BuzzerBeaterVideo from "@images/buzzerbeater.webp";
+import OrderSpotVideo from "@images/orderspot.webp";
+import * as Tabs from "@radix-ui/react-tabs";
+import Styles from "./ProjectCategoriesTabs.module.css";
 
 export default function ProjectCategoriesTabs() {
   return (
-    <Tabs.Root className={Styles['tabs-root']} defaultValue="tab1">
+    <Tabs.Root className={Styles["tabs-root"]} defaultValue="tab1">
       <Tabs.List aria-label="Projects by categories">
-        <Tabs.Trigger className={Styles['tabs-trigger']} value="tab1">
+        <Tabs.Trigger className={Styles["tabs-trigger"]} value="tab1">
           All
         </Tabs.Trigger>
-        <span className={Styles['tabs-trigger-divider']}>∙</span>
-        <Tabs.Trigger className={Styles['tabs-trigger']} value="tab2">
+        <span className={Styles["tabs-trigger-divider"]}>∙</span>
+        <Tabs.Trigger className={Styles["tabs-trigger"]} value="tab2">
           B2B
         </Tabs.Trigger>
-        <span className={Styles['tabs-trigger-divider']}>∙</span>
-        <Tabs.Trigger className={Styles['tabs-trigger']} value="tab3">
+        <span className={Styles["tabs-trigger-divider"]}>∙</span>
+        <Tabs.Trigger className={Styles["tabs-trigger"]} value="tab3">
           APP
         </Tabs.Trigger>
-        <span className={Styles['tabs-trigger-divider']}>∙</span>
-        <Tabs.Trigger className={Styles['tabs-trigger']} value="tab4">
+        <span className={Styles["tabs-trigger-divider"]}>∙</span>
+        <Tabs.Trigger className={Styles["tabs-trigger"]} value="tab4">
           Side Project
         </Tabs.Trigger>
-        <span className={Styles['tabs-trigger-divider']}>∙</span>
-        <Tabs.Trigger className={Styles['tabs-trigger']} value="tab5">
+        <span className={Styles["tabs-trigger-divider"]}>∙</span>
+        <Tabs.Trigger className={Styles["tabs-trigger"]} value="tab5">
           Design System
         </Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content className={Styles['tabs-content']} value="tab1">
+      <Tabs.Content className={Styles["tabs-content"]} value="tab1">
         <a className={Styles.project}>
           <img src={AimmoRevampImage.src}></img>
         </a>
@@ -37,10 +37,10 @@ export default function ProjectCategoriesTabs() {
           <img src={AimmoPaymentsImage.src}></img>
         </a>
         <a className={Styles.project}>
-          <img src={OrderSpotAppImage.src}></img>
+          <img src={OrderSpotVideo.src} className={Styles.verticalVideo}></img>
         </a>
         <a className={Styles.project}>
-          <img src={BuzzerBeaterMainImage.src}></img>
+          <img src={BuzzerBeaterVideo.src}></img>
         </a>
       </Tabs.Content>
     </Tabs.Root>
